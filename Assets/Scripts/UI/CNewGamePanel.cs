d1d44e1c7e88a0bcda54804f7b410828f1df0c94
Nick R <nro1912@gmail.com>
2024-10-17 16:55:29 +0300

    add choice origin

--- Assets/Scripts/UI/CNewGamePanel.cs
+++ Assets/Scripts/UI/CNewGamePanel.cs
@@ -5,6 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class CNewGamePanel : CUI
 {
+    [SerializeField] private CTextLocalize textOrigin;
+    [SerializeField] private CTextLocalize textClass;
+    private int origin;
+    private ELocalStringID[] origins = {
+        ELocalStringID.game_origin_peasant,
+        ELocalStringID.game_origin_artisan,
+        ELocalStringID.game_origin_noble };
+    private ELocalStringID[] classes = { 
+        ELocalStringID.game_class_lumberjack, ELocalStringID.game_class_hunter, ELocalStringID.game_class_acolyte, ELocalStringID.game_class_adept,
+        ELocalStringID.game_class_pikeman, ELocalStringID.game_class_crossbowman, ELocalStringID.game_class_acolyte, ELocalStringID.game_class_adept,
+        ELocalStringID.game_class_knight, ELocalStringID.game_class_duelist, ELocalStringID.game_class_acolyte, ELocalStringID.game_class_adept };
+
     private void Start()
     {
         InitUI();
@@ -20,5 +32,21 @@ public class CNewGamePanel : CUI
     {
         CGameManager.SetGameData(null);
         SceneManager.LoadScene("SceneLogo");
+    }
+
+    // on select origin
+    public void OnOrigin(int _id)
+    {
+        int n;
+
+        origin = _id / 10;
+        textOrigin.strID = origins[origin].ToString();
+        n = _id % 10;
+        n--;
+        n += origin * 4;
+        textClass.strID = classes[n].ToString();
+
+        textOrigin.RefreshText();
+        textClass.RefreshText();
     }
 }
