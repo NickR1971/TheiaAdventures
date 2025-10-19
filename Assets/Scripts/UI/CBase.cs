9dcb1830bf009b384b39095b9a13464f77f0dbaa
Nick R <nro1912@gmail.com>
2025-10-20 02:56:37 +0300

    add select dungeon ID

--- Assets/Scripts/UI/CBase.cs
+++ Assets/Scripts/UI/CBase.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CBase : CUI
 {
+    [SerializeField] private Text textID;
+    [SerializeField] private InputField newID;
     private void Start()
     {
         InitUI();
         CGameManager.GetData().num_scene = 1;
+        textID.text = CGameManager.GetData().id.ToString();
+        newID.text = CGameManager.GetData().id.ToString();
     }
-
+    public void OnNewID()
+    {
+        int vID;
+        if (int.TryParse(newID.text, out vID))
+        {
+            CGameManager.GetData().id = (uint)vID;
+            textID.text = CGameManager.GetData().id.ToString();
+        }
+    }
     public void ToBattle()
     {
         SceneManager.LoadScene("SceneBattle");
